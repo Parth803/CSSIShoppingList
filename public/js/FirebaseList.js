1f789b6be1087eb59b6bc148d200f18dae27bfde
Lucas Ancieta <lucasvancieta@gmail.com>
2021-08-02 19:04:30 +0000

    firebase javascript

--- public/js/FirebaseList.js
+++ public/js/FirebaseList.js
@@ -12,16 +12,43 @@ window.onload = (event) => {
   });
 };
 
+const getItems = (userId) => {
+  let dataArr = [];    
+  const notesRef = firebase.database().ref(`users/${userId}`);
+  notesRef.on('value', (snapshot) => {
+      dataArr.push(snapshot.val());
+  });
+  return dataArr;
+};
+
 function updateList(){
   // 1. Capture the form data
   const ingredient = document.querySelector('#searchVal');
+  const quantity = checkQuant(ingredient);
   // 2. Format the data and write it to our database
   firebase.database().ref(`users/${googleUser.uid}`).push({
     name: ingredient.value,
+    quantity: quantity,
   })
   // 3. Clear the form so that we can write a new note
   .then(() => {
-    noteTitle.value = "";
-    noteText.value = "";
+    ingredient.value = "";
   });
+}
+
+function checkQuant(name){
+    let quantity = 0;
+    const data = getItems(googleUser);
+    for(item in data){
+        if(item==name){
+            quantity++;
+            deleteNote(item);            
+        }
+    }
+    return quantity;
+}
+
+function deleteNote(noteId) {
+    firebase.database().ref(`users/${googleUserId}/${noteId}`).remove();
+    console.log("deleted ", noteId);
 }
